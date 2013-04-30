94153ccf7c4f2443eec6941bac9886f7fe17a67d
robert <rhsu123@gmail.com>
2013-04-30 12:05:12 -0400

    Initial Commit

---
+++ src/demo/ColumnTester.java
@@ -0,0 +1,17 @@
+package demo;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ */
+public class ColumnTester extends JPanel
+{
+	protected JTextField textField;
+	
+	public ColumnTester()
+	{
+		
+	}
+}
