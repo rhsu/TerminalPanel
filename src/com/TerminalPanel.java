c4c4e5e9415206f46f6be6445318a34b5a0282b2
robert <rhsu123@gmail.com>
2013-05-10 13:45:58 -0400

    some cleanup

--- src/com/TerminalPanel.java
+++ src/com/TerminalPanel.java
@@ -2,7 +2,6 @@ package com;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JTextArea;
 
 /**
  *A terminal panel GUI. To best use this, extend this class and override the 
